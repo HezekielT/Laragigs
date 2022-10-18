5fe44492132f59772d5f70dd3c197fff0411bb7f
hizkielT <hezekiel.feyissa@gmail.com>
2022-10-18 11:45:46 +0300

    listing.php

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,33 +1,11 @@
 <?php
-  namespace App\Models;
 
-  class Listing {
-    public static function all() {
-      return [
-        [
-          'id' => 1,
-          'title' => 'Listing One',
-          'description' => 'Lorem ipsum dolor Lorem ipsum dolor 
-          Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
-          Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor'
-        ],
-        [
-          'id' => 2,
-          'title' => 'Listing Two',
-          'description' => 'Lorem ipsum dolor Lorem ipsum dolor 
-          Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor
-          Lorem ipsum dolor Lorem ipsum dolor Lorem ipsum dolor'
-        ]
-      ];
-    }
+namespace App\Models;
 
-    public static function find($id) {
-      $listings = self::all();
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-      foreach($listings as $listing) {
-        if($listing['id'] == $id) {
-          return $listing;
-        }
-      }
-    }
-  }
+class Listing extends Model
+{
+    use HasFactory;
+}
