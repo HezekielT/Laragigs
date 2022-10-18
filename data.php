ae87daa6e51b923c7272dd285f7518d97bd78ac1
hizkielT <hezekiel.feyissa@gmail.com>
2022-10-18 11:45:37 +0300

    data.php

---
+++ data.php
@@ -0,0 +1,74 @@
+<?php
+
+  return [
+    [
+      'title' => 'Laravel Senior Developer',
+      'tags' => 'laravel, javascript',
+      'company' => 'Acme Corp',
+      'location' => 'Boston, MA',
+      'email' => 'email1@email.com',
+      'website' => 'https://www.acme.com',
+      'description' => 'Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ipsam minima et illo
+      reprehenderit quas possimus voluptas 
+      repudiandae cum expedita, eveniet aliquid, quam
+      illum quaerat consequatur! Expedita ab
+      consectetur tenetur delensiti?'
+    ],
+    [
+      'title' => 'React Senior Developer',
+      'tags' => 'React, javascript',
+      'company' => 'Acme Corp',
+      'location' => 'Boston, MA',
+      'email' => 'email1@email.com',
+      'website' => 'https://www.acme.com',
+      'description' => 'Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ipsam minima et illo
+      reprehenderit quas possimus voluptas 
+      repudiandae cum expedita, eveniet aliquid, quam
+      illum quaerat consequatur! Expedita ab
+      consectetur tenetur delensiti?'
+    ],
+    [
+      'title' => 'Full-Stack Senior Developer',
+      'tags' => 'laravel, Nodejs, javascript',
+      'company' => 'Acme Corp',
+      'location' => 'Boston, MA',
+      'email' => 'email1@email.com',
+      'website' => 'https://www.acme.com',
+      'description' => 'Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ipsam minima et illo
+      reprehenderit quas possimus voluptas 
+      repudiandae cum expedita, eveniet aliquid, quam
+      illum quaerat consequatur! Expedita ab
+      consectetur tenetur delensiti?'
+    ],
+    [
+      'title' => 'Nodejs Junior Developer',
+      'tags' => 'nodejs, javascript',
+      'company' => 'Acme Corp',
+      'location' => 'Boston, MA',
+      'email' => 'email1@email.com',
+      'website' => 'https://www.acme.com',
+      'description' => 'Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ipsam minima et illo
+      reprehenderit quas possimus voluptas 
+      repudiandae cum expedita, eveniet aliquid, quam
+      illum quaerat consequatur! Expedita ab
+      consectetur tenetur delensiti?'
+    ],
+    [
+      'title' => 'Backend Developer',
+      'tags' => 'python, javascript',
+      'company' => 'Acme Corp',
+      'location' => 'Boston, MA',
+      'email' => 'email1@email.com',
+      'website' => 'https://www.acme.com',
+      'description' => 'Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Ipsam minima et illo
+      reprehenderit quas possimus voluptas 
+      repudiandae cum expedita, eveniet aliquid, quam
+      illum quaerat consequatur! Expedita ab
+      consectetur tenetur delensiti?'
+    ]
+  ];
